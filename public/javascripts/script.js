126d15986019ba867851af8295d2d496713a7f3f
DaiTranNguyen049 <dai.tran.nguyen.049@gmail.com>
2026-04-06 11:35:30 +0930

    I've finished the slideshow and Mail List Box in Footer

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -19,3 +19,22 @@ document.addEventListener("click", () => { // listens for click and removes
         dropdown.classList.remove("active");
     });
 });
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  // Hide all
+  }
+  slides[slideIndex-1].style.display = "block"; // Show current
+}
