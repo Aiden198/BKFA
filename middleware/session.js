724c66611330a216b661afc87fc956433ef8175f
Aiden <a1891958@adelaide.edu.au>
2026-06-06 13:20:08 +0000

    added middleware and thing to test

---
+++ middleware/session.js
@@ -0,0 +1,18 @@
+const session = require('express-session');
+
+// USING A MEMORY STORE DATA WILL NOT PERSIST ON RESTART
+
+const sessionMiddleware = session({
+    secret: process.env.session_secret || "default",
+    resave: true, // Do not resave session if not modified
+    saveUninitialized: false, // Do not store uninitialised sessions
+    rolling: true, // Refresh expiry on each request
+    cookie: {
+        httpOnly: true, // Prevent javascript access to cookie
+        maxAge: 1000 * 60 * 60 * 24 * 30 , // 1 months expiration
+        sameSite: 'Strict', // Prevent CSRF attacks
+        secure: false
+    }
+});
+
+module.exports = sessionMiddleware;
